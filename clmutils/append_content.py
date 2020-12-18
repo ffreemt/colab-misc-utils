808b7d19b1e49a987b168adaf60c2c72c4fa9d5d
freemt <yucongo+fmt@gmail.com>
2020-12-18 16:10:00 +0800

    init cmomit

---
+++ clmutils/append_content.py
@@ -0,0 +1,51 @@
+"""Create a file."""
+from typing import Optional, Union
+
+from pathlib import Path
+import re
+# import subprocess as sp  # noqa: F401
+import chardet
+from logzero import logger
+
+from clmutils import create_file
+
+
+# fmt: off
+def append_content(
+        content: str = "",
+        dest: Union[Path, str] = Path("~/.ssh/authorized_keys").expanduser(),
+) -> Optional[Path]:
+    # fmt: on
+    """Append content to a file.
+
+    Create the file if it does not exist.
+    """
+    # unticipate people from another world
+    pattern = re.compile(r"\$home|%userprofile%?", re.IGNORECASE)
+    if isinstance(dest, str):
+        dest = pattern.sub("~", dest)
+    else:
+        _ = dest.__str__()
+        dest = pattern.sub("~", _)
+    dest = Path(dest).expanduser().resolve()
+
+    if not dest.exists():
+        try:
+            create_file("", dest)
+        except Exception as exc:
+            logger.error("create_file exc: %s", exc)
+
+    # detect encoding and read
+    _ = dest.read_bytes()
+    encoding = chardet.detect(_).get("encoding")
+    c_content = Path(dest).read_text(encoding)
+
+    # attach content and write back in utf8
+    _ = f"{c_content}\n{content}"
+    try:
+        Path(dest).write_text(_, "utf8")
+    except Exception as exc:
+        logger.error("write_text exc: %s", exc)
+        return None
+
+    return dest
---
+++ clmutils/create_file.py
@@ -0,0 +1,51 @@
+"""Create a file."""
+from typing import Optional, Union
+
+from pathlib import Path
+import re
+
+# import subprocess as sp  # noqa: F401
+from logzero import logger
+
+
+# fmt: off
+def create_file(
+        content: str = "",
+        dest: Union[Path, str] = Path("~/.ssh/gh-key").expanduser(),
+        mode: Optional[Union[str, int]] = None,
+        overwrite: bool = False,
+) -> Optional[Path]:
+    # fmt: on
+    """Create a file if it does not exist.
+
+    Overwrite existing file only if set to True.
+    """
+    # unticipate people from another world
+    pattern = re.compile(r"\$home|%userprofile%?", re.IGNORECASE)
+    if isinstance(dest, str):
+        dest = pattern.sub("~", dest)
+    else:
+        _ = dest.__str__()
+        dest = pattern.sub("~", _)
+    dest = Path(dest).expanduser().resolve()
+
+    if dest.exists() and overwrite is False:
+        logger.warning(" %s exists and overwrite set to False", dest)
+        logger.warning("Will not proceed")
+        return None
+    p_dir = dest.parent
+
+    try:
+        p_dir.mkdir(parents=True, exist_ok=True)
+    except Exception as exc:
+        logger.error("p_dir.mkdir exc: %s", exc)
+        logger.warning("Will not proceed")
+        return None
+
+    try:
+        dest.write_text(content, encoding="utf8")
+    except Exception as exc:
+        logger.error("dest.write_text exc: %s", exc)
+        return None
+
+    return dest
